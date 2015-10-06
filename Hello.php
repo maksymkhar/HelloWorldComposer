782cbdf401eee84fcf6afab4bac684524aedb511
Max <maksymkharuk@iesebre.com>
2015-10-06 19:45:13 +0200

    First commit

---
+++ Hello.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 6/10/15
+ * Time: 19:31
+ */
+
+class Hello
+{
+    public function sayHello()
+    {
+        echo "Hola mon!\n";
+    }
+
+}
---
+++ Main.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: max
+ * Date: 6/10/15
+ * Time: 19:32
+ */
+
+require __DIR__ . "/Hello.php";
+
+$greetings = new Hello();
+$greetings->sayHello();
